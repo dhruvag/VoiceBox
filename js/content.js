2752bf11995656e2dd329e9c0b7fce418bf65d1c
shauryarjain <shauryarjain@gmail.com>
2014-11-15 06:09:44 -0500

    added voice recording on gmail, prints to console

--- js/content.js
+++ js/content.js
@@ -2,6 +2,18 @@ window.onload = function () {
 myButton = document.createElement("input");
 myButton.type = "button";
 myButton.value = "Say command";
-placeHolder = document.getElementsByClassName("Cq aqL")[0];
+placeHolder = document.getElementsByClassName("nH aqK")[0];
 placeHolder.appendChild(myButton);
+
+
+var recognition = new webkitSpeechRecognition();
+var i = 0;
+recognition.continuous = true;
+// recognition.interimResults = false;
+recognition.onresult = function(event) { 
+  console.log(event);
+  // console.log(event["results"][i][0]); // print in JS
+  i++;
+}
+recognition.start();
 }
