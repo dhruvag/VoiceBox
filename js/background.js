adc83ff3fd7056d43950291456dca2289dbb7e2e
David Lakata <dlakata@seas.upenn.edu>
2014-11-15 04:06:20 -0500

    adds a button to gmail

--- js/background.js
+++
@@ -1,8 +0,0 @@
-// Convert MIME type of .tex files to text/plain
-chrome.webRequest.onHeadersReceived.addListener(function(details){
-    for(var i = 0; i < details.responseHeaders.length; ++i)
-        if(details.responseHeaders[i].name.toLowerCase() == 'content-type')
-            details.responseHeaders[i].value = 'text/plain';
-    return {responseHeaders:details.responseHeaders};
-}, {urls: ["http://*/*.tex"]}, ['blocking', 'responseHeaders']);
-
--- js/content.js
+++ js/content.js
@@ -1,11 +1,7 @@
-var pdfElement = document.createElement("div");
-var baseURL = window.location.href.slice(0,-4) + ".pdf";
-var renderedPDF = "http://104.41.129.152/" + baseURL;
-pdfElement.innerHTML = '<iframe src="' + renderedPDF + '" style="position:fixed; top:0; right:0" width="50%" height="100%"></iframe>';
-document.body.appendChild(pdfElement);
-var latex = document.getElementsByTagName('pre')[0];
-
-// CSS styling for LaTeX pane
-latex.style.width = "50%";
-latex.style.float = "left";
-
+window.onload = function () {
+myButton = document.createElement("input");
+myButton.type = "button";
+myButton.value = "Say command";
+placeHolder = document.getElementsByClassName("Cq aqL")[0];
+placeHolder.appendChild(myButton);
+}
