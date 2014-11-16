a5c1688d7f344d69dc6a1b624f56586a45eb28ad
shauryarjain <shauryarjain@gmail.com>
2014-11-16 04:53:56 -0500

    speech final changes

--- js/content.js
+++ js/content.js
@@ -82,13 +82,173 @@ function processCommand(command) {
         console.log('attachments');
         document.location.href = "https://mail.google.com/mail/u/0/#search/has%3Aattachment";
     }
-    else if (command.indexOf('speak') > -1) {
-        console.log($('.a3s').text());
-        var u = new SpeechSynthesisUtterance();
-        u.text = $('.a3s').text();
-        window.speechSynthesis.speak(u);
+    else if (command.indexOf('preview') > -1) {
+	   	// helper function to parse long text
+		var speechUtteranceChunker = function (utt, settings, callback) {
+			settings = settings || {};
+			var newUtt;
+			var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
+			if (utt.voice && utt.voice.voiceURI === 'native') { // Not part of the spec
+				newUtt = utt;
+				newUtt.text = txt;
+				newUtt.addEventListener('end', function () {
+				    if (speechUtteranceChunker.cancel) {
+				        speechUtteranceChunker.cancel = false;
+				    }
+				    if (callback !== undefined) {
+				        callback();
+				    }
+				});
+			} else {
+				var chunkLength = (settings && settings.chunkLength) || 160;
+				var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
+				var chunkArr = txt.match(pattRegex);
+				if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
+			    //call once all text has been spoken...
+			    	if (callback !== undefined) {
+			        	callback();
+			    	}
+			    	return;
+				}
+				var chunk = chunkArr[0];
+				newUtt = new SpeechSynthesisUtterance(chunk);
+				var x;
+				for (x in utt) {
+				    if (utt.hasOwnProperty(x) && x !== 'text') {
+				        newUtt[x] = utt[x];
+				    }
+				}
+				newUtt.addEventListener('end', function () {
+			    	if (speechUtteranceChunker.cancel) {
+			        	speechUtteranceChunker.cancel = false;
+			        	return;
+			    	}
+				    settings.offset = settings.offset || 0;
+				    settings.offset += chunk.length - 1;
+				    speechUtteranceChunker(utt, settings, callback);
+	        	});
+			}	
+
+		    if (settings.modifier) {
+		        settings.modifier(newUtt);
+		    }
+		    console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
+		    //placing the speak invocation inside a callback fixes ordering and onend issues.
+		    setTimeout(function () {
+		        speechSynthesis.speak(newUtt);
+		    }, 0);
+		};
+
+		// var previewstate = 1;
+		var divarr = $(".afn"); // unread, from, subject, and preview date?
+		// console.log(divarr.text());
+		console.log(divarr);
+		var longtext = "";
+		for(var i=0; i < divarr.length; i++ ) {
+			longtext += divarr[i].textContent;
+		}
+
+		// console.log($('.a3s').text());
+         var u = new SpeechSynthesisUtterance();
+         var voices = speechSynthesis.getVoices();
+         u.default = false;
+	     u.voice = voices[1];
+	     u.voiceURI = 'native';
+	     u.volume = 1;
+	     u.rate = 1;
+	     u.pitch = 2;
+	     u.lang = 'en-US';
+         u.text = longtext;
+         // console.log(u);
+         // window.speechSynthesis.speak(u);
+         
+     	speechUtteranceChunker(u, {
+		    chunkLength: 120
+		}, function () {
+		    //some code to execute when done
+		    console.log('done');
+		});
     }
-  }
+
+    else if (command.indexOf('speak') > -1) {
+   	
+   	// helper function to parse long text
+	    var speechUtteranceChunker = function (utt, settings, callback) {
+		    settings = settings || {};
+		    var newUtt;
+		    var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
+		    if (utt.voice && utt.voice.voiceURI === 'native') { // Not part of the spec
+		        newUtt = utt;
+		        newUtt.text = txt;
+		        newUtt.addEventListener('end', function () {
+		            if (speechUtteranceChunker.cancel) {
+		                speechUtteranceChunker.cancel = false;
+		            }
+		            if (callback !== undefined) {
+		                callback();
+		            }
+		        });
+		    } else {
+		        var chunkLength = (settings && settings.chunkLength) || 160;
+		        var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
+		        var chunkArr = txt.match(pattRegex);
+
+		        if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
+		            //call once all text has been spoken...
+		            if (callback !== undefined) {
+		                callback();
+		            }
+		            return;
+		        }
+		        var chunk = chunkArr[0];
+		        newUtt = new SpeechSynthesisUtterance(chunk);
+		        var x;
+		        for (x in utt) {
+		            if (utt.hasOwnProperty(x) && x !== 'text') {
+		                newUtt[x] = utt[x];
+		            }
+		        }
+		        newUtt.addEventListener('end', function () {
+		            if (speechUtteranceChunker.cancel) {
+		                speechUtteranceChunker.cancel = false;
+		                return;
+		            }
+		            settings.offset = settings.offset || 0;
+		            settings.offset += chunk.length - 1;
+		            speechUtteranceChunker(utt, settings, callback);
+		        });
+			}	
+
+		    if (settings.modifier) {
+		        settings.modifier(newUtt);
+		    }
+		    console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
+		    //placing the speak invocation inside a callback fixes ordering and onend issues.
+		    setTimeout(function () {
+		        speechSynthesis.speak(newUtt);
+		    }, 0);
+		};
+
+        console.log($('.a3s').text());
+         var u = new SpeechSynthesisUtterance();
+         var voices = speechSynthesis.getVoices();
+	     u.voice = voices[1];
+	     u.voiceURI = 'native';
+	     u.volume = 1;
+	     u.rate = 1;
+	     u.pitch = 2;
+	     u.lang = 'en-US';
+         u.text = $('.a3s').text();
+         console.log(u);
+
+     	speechUtteranceChunker(u, {
+		    chunkLength: 120
+		}, function () {
+		    //some code to execute when done
+		    console.log('done');
+		});
+    }
+}
 
 function processLabel(label) {
     console.log('label');
